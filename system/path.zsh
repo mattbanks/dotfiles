467f3889da856a463b036a265d0de5fa442d7a02
Matt Banks <mjbanks@gmail.com>
2013-02-18 20:09:09 -0500

    change node path based on brew install, moved rvm path here since it's where it belongs

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,7 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-export NODE_PATH="$NODE_PATH:/usr/local/lib/node_modules"
+export NODE_PATH="$NODE_PATH:/usr/local/share/npm/bin"
+
+export RVM_PATH="$PATH:$HOME/.rvm/bin"
