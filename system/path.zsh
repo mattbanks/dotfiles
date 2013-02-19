455548dc725e39b51382636defae96973ddda0ee
Matt Banks <mjbanks@gmail.com>
2013-02-18 20:21:52 -0500

    fix path

--- system/path.zsh
+++ system/path.zsh
@@ -2,6 +2,8 @@ export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-export NODE_PATH="$PATH:/usr/local/share/npm/bin"
+# Add Node to PATH
+export PATH="$PATH:/usr/local/share/npm/bin"
 
-export RVM_PATH="$PATH:$HOME/.rvm/bin"
+# Add RVM to PATH
+export PATH="$PATH:$HOME/.rvm/bin"
