d47a4f6436e85e233276a3485c4f12cdd9fadd61
Matt Banks <mjbanks@gmail.com>
2013-02-20 09:41:48 -0500

    kill Finder and reopen TotalFinder after fixopenwith

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -101,4 +101,4 @@ alias rot13='tr a-zA-Z n-za-mN-ZA-M'
 
 # Quick way to rebuild the Launch Services database and get rid
 # of duplicates in the Open With submenu.
-alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user; killall Finder; open -a TotalFinder'
