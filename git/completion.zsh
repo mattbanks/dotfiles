c40b182c19b13bd896b9e96b3826e3d0767c8113
Matt Banks <mjbanks@gmail.com>
2013-02-20 21:02:39 -0500

    update git completion script location based on brew zsh completion

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,7 +1,8 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+# completion=/usr/local/etc/bash_completion.d/git-completion.bash
+completion=/usr/local/share/zsh/site-functions/git-completion.bash
 
 if test -f $completion
 then
