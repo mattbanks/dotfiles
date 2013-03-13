7677af4ec8ce2deddccedce25b513c691dbed7a3
Matt Banks <mjbanks@gmail.com>
2013-03-13 11:44:46 -0400

    add gdt alias for git difftool and gt for git tag

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,12 +11,14 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gdt='git difftool'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gt="git tag -a"
 
 # Undo a `git push`
 #alias undopush="git push -f origin HEAD^:master"
