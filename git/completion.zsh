25d69baf629183888dfa6cb274d7e7c0693ec2e2
Matt Banks <mjbanks@gmail.com>
2013-06-19 10:54:29 -0400

    update based on holman's dotfiles

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,9 +1,9 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/share/zsh/site-functions/_git
+completion='$(brew --prefix)/share/zsh/site-functions/_git'
 
-if [[ -f $completion ]];
+if test -f $completion
 then
-  export FPATH="$completion:$FPATH"
+  source $completion
 fi
