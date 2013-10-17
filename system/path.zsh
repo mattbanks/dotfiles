9dbd75dbfcc2c8faa1557aa165f058341cb8b089
Matt Banks <mjbanks@gmail.com>
2013-10-17 09:33:38 -0400

    tweaks based on latest holman dotfiles

--- system/path.zsh
+++ system/path.zsh
@@ -1,13 +1,6 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Add Node to PATH
 export PATH="$PATH:/usr/local/share/npm/bin"
-
-# Add RVM to PATH
-# export PATH="$PATH:$HOME/.rvm/bin"
-
-# Add WP-CLI to PATH
-# export PATH="$PATH:$HOME/.wp-cli/bin"
-# export WP_CLI_PHP=/usr/bin/php
