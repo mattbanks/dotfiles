7023b04c8a15ef0a7700bdf992289394172befd7
Matt Banks <mjbanks@gmail.com>
2013-10-24 17:44:21 -0400

    fix xcode setup, remove wp-cli bash completions - using through vagrant

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,8 +3,3 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
-
-# WP-CLI Bash completions
-autoload bashcompinit
-bashcompinit
-source $HOME/.wp-cli/vendor/wp-cli/wp-cli/utils/wp-completion.bash
