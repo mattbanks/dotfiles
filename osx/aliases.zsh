65c9b16fe5002ec1f804d3ed05d601e8c0df83ea
Matt Banks <mjbanks@gmail.com>
2013-10-28 11:59:57 -0400

    fix show/hide hidden files aliases to work in mavericks

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -8,12 +8,13 @@ alias resetevents="sudo killall -KILL appleeventsd"
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; rm -rfv ~/.Trash"
 
 # Show/hide hidden files in Finder
-alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
 
 # Hide/show all desktop icons (useful when presenting)
 alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
 alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
 
 # Disable Gatekeeper
 alias gateoff="sudo spctl --master-disable"
