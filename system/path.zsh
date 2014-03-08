55fd833ef932b4579e4de59d1cd64b44385e971d
Matt Banks <mjbanks@gmail.com>
2014-03-08 11:29:37 -0500

    add wp-cli php from mamp

--- system/path.zsh
+++ system/path.zsh
@@ -4,3 +4,6 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Add Node to PATH
 export PATH="$PATH:/usr/local/share/npm/bin"
+
+# Set WP-CLI PHP to MAMP Pro 3 PHP 5.5
+export WP_CLI_PHP="/Applications/MAMP/bin/php/php5.5.9/bin/php"
