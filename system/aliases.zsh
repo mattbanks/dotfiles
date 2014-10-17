3a9579664ced1c0cbb14ef1465e0ff45b353928f
Matt Banks <mjbanks@gmail.com>
2014-10-17 09:15:14 -0400

    remove hub alias, add sudo for gem updates

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -72,7 +72,6 @@ alias st="subl"
 alias stt="subl ."
 alias t="tar -pcvzf"
 alias tx="tar -pvxf"
-alias git="hub"
 alias g="git"
 alias v="vim"
 alias o="open"
@@ -83,8 +82,8 @@ alias sudo='sudo '
 
 # Homebrew, NPM, RVM, Gems Update, Cleanup Brew and Gems
 alias npmup='npm -g list --depth 0 | grep -v "^/" | cut -f2 -d" " | cut -f1 -d@ | grep -v "^npm$" | xargs npm -g update'
-alias pkgup="brew update; brew upgrade; npmup; gem update --system; gem update;"
-alias pkgclean="brew cleanup; npm cache clean; gem cleanup;"
+alias pkgup="brew update; brew upgrade; npmup; sudo gem update --system; sudo gem update;"
+alias pkgclean="brew cleanup; npm cache clean; sudo gem cleanup;"
 
 # Edit hosts file and vhosts config for local development
 # alias edithost="sudo st /private/etc/hosts"
