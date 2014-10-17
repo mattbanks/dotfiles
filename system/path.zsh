7ade8bd6e17c41b6b18fad2e9d9a253481b4c888
Matt Banks <mjbanks@gmail.com>
2014-10-17 09:15:23 -0400

    take rbenv out of path

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
