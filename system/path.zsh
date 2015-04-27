d097d30e9cd24abaeee0a1489018243dec6b5592
Matt Banks <mjbanks@gmail.com>
2015-04-27 09:36:00 -0400

    use latest php with mamp pro for wp cli, misc updates

--- system/path.zsh
+++ system/path.zsh
@@ -6,5 +6,5 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH="$PATH:/usr/local/share/npm/bin"
 
 # Set WP-CLI PHP to MAMP Pro 3 PHP 5.5
-export MAMP_PHP="/Applications/MAMP/bin/php/php5.5.18/bin"
+export MAMP_PHP="/Applications/MAMP/bin/php/$(ls /Applications/MAMP/bin/php/ | tail -1)/bin"
 export PATH="$MAMP_PHP:$PATH"
