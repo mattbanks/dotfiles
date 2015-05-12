ba17b9506e58fe08c715d72b5935a742a2016741
Matt Banks <mjbanks@gmail.com>
2015-05-12 09:14:15 -0400

    add --all to brew upgrade

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -82,7 +82,7 @@ alias oo="open ."
 alias sudo='sudo '
 
 # Homebrew, NPM, Gems Update, Cleanup Brew and Gems
-alias pkgup="brew update; brew upgrade; npm-upgrade; gem update --system; gem update;"
+alias pkgup="brew update; brew upgrade --all; npm-upgrade; gem update --system; gem update;"
 alias pkgclean="brew cleanup; npm cache clean; gem cleanup;"
 
 # Edit hosts file and vhosts config for local development
