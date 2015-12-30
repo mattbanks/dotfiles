646173977d4e55d59e1aa6c108224b7fde09d327
Matt Banks <mjbanks@gmail.com>
2015-12-30 10:10:13 -0500

    remove old path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,8 +1,5 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-# Old PATH
-# export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
 # Add Node to PATH
 export PATH="$PATH:/usr/local/share/npm/bin"
