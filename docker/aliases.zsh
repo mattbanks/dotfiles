1a582bbf7b6b7e6a06b5a042af7cf8e35449fcc8
Matt Banks <mjbanks@gmail.com>
2016-02-11 11:07:15 -0500

    add docker aliases, thanks @chiefy

---
+++ docker/aliases.zsh
@@ -0,0 +1,25 @@
+#!/bin/sh
+export DOCKER_MACHINE_NAME=docker-host
+
+alias dm="docker-machine"
+alias dc="docker-compose"
+alias dmr="docker-machine restart $DOCKER_MACHINE_NAME && reload-docker"
+alias di="docker images"
+alias dps="docker ps"
+alias dcps="docker-compose ps"
+
+drmi() {
+  images=$(docker images -q -f dangling=true)
+  if [[ -z "$images" ]]; then
+     echo "No images to delete."
+  else
+    docker rmi -f $(printf " %s" "${images[@]}")
+  fi
+}
+
+reload-docker() {
+  eval "$(docker-machine env $DOCKER_MACHINE_NAME)"
+}
+
+# invoke this immeadiately
+reload-docker
