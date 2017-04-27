25fe8b687a03bf66774264f2f8c1522cfc1edd8f
Matt Banks <mjbanks@gmail.com>
2017-04-26 21:42:05 -0400

    git updates

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,7 +10,9 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias gd='git diff'
+
+# Remove `+` and `-` from start of diff lines; just rely upon color.
+alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 alias gdt='git difftool'
 alias gc='git commit'
 alias gca='git commit -a'
