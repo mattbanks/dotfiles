887393bf9c99a840973ebdd0ef05ab1774bf30db
Matt Banks <mjbanks@gmail.com>
2017-05-17 09:46:20 -0400

    cleanup old completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -4,19 +4,5 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
-# Load Bash completions
-# autoload bashcompinit
-# bashcompinit
-#
-# # WP-CLI Bash completions from Homebrew
-# if [ -f ~/.dotfiles/wp-cli/wp-completion.bash ]; then
-# 	. ~/.dotfiles/wp-cli/wp-completion.bash
-# fi
-
-# NPM Bash completions from Homebrew
-# if [ -f $(brew --prefix)/etc/bash_completion.d/npm ]; then
-# 	. $(brew --prefix)/etc/bash_completion.d/npm
-# fi
-
 # AWS CLI completions from Homebrew
 source /usr/local/share/zsh/site-functions/_aws
