e63e5bcc9e173150c599d374bf9600c97897edd5
Matt Banks <mjbanks@gmail.com>
2017-05-17 21:29:56 -0400

    add express-generator

--- node/install.sh
+++ node/install.sh
@@ -3,4 +3,4 @@
 # Install npm packages
 
 npm install -g npm
-npm install -g eslint gulp jshint n npm-check-updates pure-prompt sequelize-cli xo
+npm install -g eslint express-generator gulp jshint n npm-check-updates pure-prompt sequelize-cli xo
