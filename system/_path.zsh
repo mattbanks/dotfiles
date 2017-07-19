a015a3d301f54fe7fdf52ffbf3a43046ebbe0c9c
Matt Banks <mjbanks@gmail.com>
2017-07-18 20:44:31 -0400

    update path for homebrew installed python

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,6 @@
+# Homebrew python
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+# All dotfiles scripts, Homebrew installed packages, etc
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
