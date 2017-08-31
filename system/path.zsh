770b35bf352308b1544284dfe8dcff9e5a3f2826
Matt Banks <mjbanks@gmail.com>
2017-08-31 13:31:08 -0400

    add ~/.bin to path

--- system/path.zsh
+++ system/path.zsh
@@ -5,4 +5,4 @@ export PATH="$PATH:$DOTFILES/bin"
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 # All dotfiles scripts, Homebrew installed packages, etc
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.bin:$PATH"
