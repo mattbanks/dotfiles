ac06802f2b5afc489e48b9dd2d705b8d37859d0c
Matt Banks <mjbanks@gmail.com>
2018-05-04 11:31:00 -0400

    use the full paths for copying vscode files

--- vscode/install.sh
+++ vscode/install.sh
@@ -6,9 +6,9 @@ if test "$(which code)"; then
 		VSCODE_HOME="$HOME/.config/Code"
 	fi
 
-	ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User"
-	ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User"
-	ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User"
+	ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+	ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+	ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
 
 	# from `code --list-extensions`
 	modules="
