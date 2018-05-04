0fd9353654ecd966257dd02d1f27c366bb918088
Matt Banks <mjbanks@gmail.com>
2018-05-04 09:13:23 -0400

    add node 8 to path for now

--- system/path.zsh
+++ system/path.zsh
@@ -4,5 +4,8 @@ export PATH="$PATH:$DOTFILES/bin"
 # Homebrew python
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
+# Homebrew Node 8, because Node 10 breaks everything right now
+export PATH="/usr/local/opt/node@8/bin:$PATH"
+
 # All dotfiles scripts, Homebrew installed packages, etc
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.bin:$PATH"
