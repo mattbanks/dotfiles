40f96e1828054cc636365d8077b6ebb5cb916f63
Matt Banks <mjbanks@gmail.com>
2018-08-07 20:39:45 -0400

    updates from upstream

--- system/path.zsh
+++ system/path.zsh
@@ -4,8 +4,5 @@ export PATH="$PATH:$DOTFILES/bin:$HOME/.bin"
 # Homebrew python
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
-# Homebrew Node 8, because Node 10 breaks everything right now
-export PATH="/usr/local/opt/node@8/bin:$PATH"
-
 # All dotfiles scripts, Homebrew installed packages, etc
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.bin:$PATH"
