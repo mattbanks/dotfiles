652f881b566309d455d984d0ca040405bff0fad5
Matt Banks <mjbanks@gmail.com>
2019-02-28 14:53:43 -0500

    various updates, pulls in latest from caarlos0 dotfiles

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -2,3 +2,7 @@
 alias d='docker'
 alias dc='docker-compose'
 alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
+
+docker_prune() {
+	docker system prune --volumes -fa
+}
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,2 +1,3 @@
 #!/bin/zsh
 PURE_CMD_MAX_EXEC_TIME=1
+export REPORTTIME=10
