8fe9fa47bfaa6bd7565eb5f16c862aa1f7865fbe
Matt Banks <mjbanks@gmail.com>
2020-11-22 20:23:01 -0500

    update ssh alias for ed35519 key

--- ssh/aliases.zsh
+++ ssh/aliases.zsh
@@ -1,2 +1,2 @@
 #!/bin/sh
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
