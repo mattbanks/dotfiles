369fb22e896cf81708045df01bbf0e886b632fb0
Matt Banks <mjbanks@gmail.com>
2025-01-15 20:29:16 -0500

    update node

--- nvm/setup.sh
+++ nvm/setup.sh
@@ -2,6 +2,6 @@
 #
 # Install nvm and latest lts
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 nvm install --lts
-nvm alias default 18
+nvm alias default 22
