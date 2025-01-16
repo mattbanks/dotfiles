f22fd625b5e376de36890d59586f289c03e722be
Matt Banks <mjbanks@gmail.com>
2025-01-15 20:29:24 -0500

    copy rsa pubkey

--- ssh/aliases.zsh
+++ ssh/aliases.zsh
@@ -1,2 +1,3 @@
 #!/bin/sh
 alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey_rsa="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
