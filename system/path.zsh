f6af4f767506c12dd880945e73109e1050a58ecb
Matt Banks <mjbanks@gmail.com>
2026-02-27 11:25:46 -0500

    add obsidian cli to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
 #!/bin/sh
 export PATH="$PATH:$DOTFILES/bin:$HOME/.bin"
 export PATH="$HOME/.local/bin:$PATH"
+
+# Add Obsidian CLI to path
+export PATH="$PATH:/Applications/Obsidian.app/Contents/MacOS"
